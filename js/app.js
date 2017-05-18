39a1e4d4fa66a4cb1f592a4085154d996340222e
Samuel Roberts <rober352@purdue.edu>
2017-05-18 10:52:47 -0400

    Add functionality for everything except last 2 extra credits

--- js/app.js
+++ js/app.js
@@ -21,6 +21,7 @@ const megaroster = {
     const student = {
       id: this.max + 1,
       name: f.studentName.value,
+      promote: false,
     }
 
     this.students.unshift(student)
@@ -47,15 +48,65 @@ const megaroster = {
       .querySelector('button.remove')
       .addEventListener('click', this.removeStudent.bind(this))
 
+    li
+      .querySelector('button.promote')
+      .addEventListener('click', this.promoteStudent.bind(this))
+
+    li
+      .querySelector('button.moveup')
+      .addEventListener('click', this.moveupStudent.bind(this))
+
+    li
+      .querySelector('button.movedown')
+      .addEventListener('click', this.movedownStudent.bind(this))
+
     return li
   },
 
   removeStudent(ev) {
     const btn = ev.target
-    btn.closest('.student').remove()
 
     // Remove it from the this.students array
-    // this.students.splice(?, 1)
+    const id = btn.closest('.student').dataset.id
+    const index = this.students.findIndex((student) => {
+      return student.id == id
+    })
+    this.students.splice(index, 1)
+
+    btn.closest('.student').remove()
+  },
+
+  promoteStudent(ev) {
+    const btn = ev.target
+    btn.closest('.student').style.border = '2px solid yellow'
+
+    console.log(btn)
+    console.log(btn.closest('.student'))
+    const id = btn.closest('.student').dataset.id
+    const index = this.students.findIndex((student) => {
+      return student.id == id
+    })
+    console.log(name)
+    console.log(index)
+    console.log(this.students)
+    this.students[index].promote = true
+  },
+
+  moveupStudent(ev) {
+      const btn = ev.target
+
+      //li > span > button
+      const span = btn.parentNode
+      const li = span.parentNode
+      const liPrevSibling = li.previousSibling
+      const ul = li.parentNode
+      ul.insertBefore(li, liPrevSibling)
+      li.remove()
+
+  },
+
+  movedownStudent(ev) {
+
   },
 
   removeClassName(el, className) {
